ed9f34b6cb4b889bc5b7cf877985c395afebd6b0
François Kooman <fkooman@tuxed.net>
2015-11-22 22:26:20 +0100

    add script to run php-remote-storage with PHP cli web server

---
+++ contrib/rs-router.php
@@ -0,0 +1,22 @@
+<?php
+
+// You can use this router to use PHP's built in server for development 
+// purposes. From the main folder run this:
+//
+// php -S localhost:8080 -t web/ contrib/rs-router.php
+//
+// Now you should be able to use RS apps using 'foo@localhost:8080' as 
+// user address in the RS widget
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI'])) {
+    return false;
+} else {
+    $questionPos = strpos($_SERVER['REQUEST_URI'], '?');
+    if (false !== $questionPos) {
+        $_SERVER['PATH_INFO'] = substr($_SERVER['REQUEST_URI'], 0, $questionPos);
+    } else {
+        $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+    }
+
+    require 'web/index.php';
+}
